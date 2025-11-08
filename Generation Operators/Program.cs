ba0ed0f4bb07e9dff97e4f8e86acb4e05e4f8a3c
mehrshad <mehrshadkhakifirooz@gmail.com>
2025-11-08 23:28:09 +0330

    repeat example

--- Generation Operators/Program.cs
+++ Generation Operators/Program.cs
@@ -1,4 +1,9 @@
 ﻿// generation (range, repeat, empty)
-var result = Enumerable.Range(1, 20).Where(x => x % 2 == 0);
+
+//var result = Enumerable.Range(1, 20).Where(x => x % 2 == 0);
+//foreach (var item in result)
+//    Console.WriteLine(item);
+
+var result = Enumerable.Repeat("Hello", 8);
 foreach (var item in result)
     Console.WriteLine(item);
