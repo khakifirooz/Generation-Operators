3702d749bdc7b2f20e3720e2168e6bd0ac95dc12
mehrshad <mehrshadkhakifirooz@gmail.com>
2025-11-08 23:33:42 +0330

    empty example added

--- Generation Operators/Program.cs
+++ Generation Operators/Program.cs
@@ -4,6 +4,14 @@
 //foreach (var item in result)
 //    Console.WriteLine(item);
 
-var result = Enumerable.Repeat("Hello", 8);
-foreach (var item in result)
-    Console.WriteLine(item);
+//var result = Enumerable.Repeat("Hello", 8);
+//foreach (var item in result)
+//    Console.WriteLine(item);
+
+var result = GetNames() ?? Enumerable.Empty<string>(); //null coalescing operator        ( it the metos was null the empty will be active )
+
+
+static IEnumerable<string> GetNames()
+{
+    return null;
+}
